3b3497674f4e3d899572428d611675cfca5c9004
ChrisBafax <chrisbafaro@icloud.com>
2023-01-26 17:48:11 +0100

    6.0.1 Added Spring Cloud Eureka.

--- Esercizio 5/PizzaService/target/generated-sources/annotations/com/myrestaurant/store/PizzaService/mapper/RestaurantIdsMapperImpl.java
+++ Esercizio 5/PizzaService/target/generated-sources/annotations/com/myrestaurant/store/PizzaService/mapper/RestaurantIdsMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-01-26T12:37:01+0100",
+    date = "2023-01-26T17:34:46+0100",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
 )
 @Component
--- Esercizio 5/RestaurantService/src/main/java/com/myrestaurant/store/RestaurantService/RestaurantServiceApplication.java
+++ Esercizio 5/RestaurantService/src/main/java/com/myrestaurant/store/RestaurantService/RestaurantServiceApplication.java
@@ -2,8 +2,10 @@ package com.myrestaurant.store.RestaurantService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RestaurantServiceApplication {
 
     public static void main(String[] args) {
