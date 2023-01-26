74034d91436af20d0b3e9959d6f394069bdd6da3
ChrisBafax <chrisbafaro@icloud.com>
2023-01-26 14:50:37 +0100

    5.0.4 Fixes.

--- Esercizio 4/NotificationService/src/main/java/com/myrestaurant/store/notificationservice/listener/NotificationListener.java
+++ Esercizio 4/NotificationService/src/main/java/com/myrestaurant/store/notificationservice/listener/NotificationListener.java
@@ -7,8 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @Slf4j
 @RequiredArgsConstructor
