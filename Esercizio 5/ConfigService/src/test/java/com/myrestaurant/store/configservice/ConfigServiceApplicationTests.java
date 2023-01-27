4fbd8e1d263e0710aaa07990e96bf49c6c74ee36
ChrisBafax <chrisbafaro@icloud.com>
2023-01-27 10:32:18 +0100

    8.0.0 ConfigService

---
+++ Esercizio 5/ConfigService/src/test/java/com/myrestaurant/store/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myrestaurant.store.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
